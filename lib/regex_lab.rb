82ed1766c373578dda0f1b6ae04158ca27b81159
Leah Schlackman <leahemily1@gmail.com>
2020-09-21 18:52:56 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,9 +3,9 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.match(/\b[aeiouAEIOU]\w*/)
-    return true 
-  # end
+ if text.match(/\b[aeiouAEIOU]\w*/)
+  end
+  return tru
 end
 
 
