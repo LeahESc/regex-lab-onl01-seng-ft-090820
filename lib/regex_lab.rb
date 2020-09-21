6ad0ecfb1a14b5ecaeea857aa0a4c05d6b4a4903
Leah Schlackman <leahemily1@gmail.com>
2020-09-21 19:10:53 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
- if text.match(/\b[aeiouAEIOU]\w*/) == matchdata
+ if text.match(/\b[aeiouAEIOU]\w*/) == true 
   return true 
   end
 end
